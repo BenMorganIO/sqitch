37c3ae0b09385968c623c6ce1016e06f9b0d2045
Ben A. Morgan <ben@benmorgan.io>
2017-10-27 11:29:54 -0400

    add otp for hex

--- lib/sqitch.ex
+++ lib/sqitch.ex
@@ -3,4 +3,12 @@ defmodule Sqitch do
   Sqitch is a semi-sane way to manage databases. This module here is all about
   integrating with the power of Sqitch.
   """
+
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    Supervisor.start_link([], strategy: :one_for_one, name: Sqitch.Supervisor)
+  end
 end
