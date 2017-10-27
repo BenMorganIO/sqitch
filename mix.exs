c1776b3aba7d3dcfcc03615ef529c11a756c5888
Ben A. Morgan <ben@benmorgan.io>
2017-10-27 11:50:08 -0400

    add package config

--- mix.exs
+++ mix.exs
@@ -8,9 +8,28 @@ defmodule Sqitch.Mixfile do
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       config_path: "config/config.exs",
-      deps: []
+      deps: [],
+      package: package()
     ]
   end
 
   def application, do: [extra_applications: [:logger]]
+
+  defp package do
+    [
+      description: """
+      Sqitch is a semi-sane way to manage databases.
+      This module here is all about integrating with the power of Sqitch.
+      """,
+      files: ~w(lib mix.exs README.md),
+      homepage_url: "https://hexdocs.pm/sqitch",
+      licenses: ["MIT"],
+      links: %{
+        github: "https://github.com/BenMorganIO/sqitch",
+        docs: "https://hexdocs.pm/sqitch"
+      },
+      maintainers: ["Ben A. Morgan <ben@benmorgan.io>"],
+      source_url: "https://github.com/BenMorganIO/sqitch"
+    ]
+  end
 end
